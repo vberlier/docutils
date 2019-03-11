2303abbe6e138b93e023a81ae57b5af7ff66b4a7
Valentin Berlier <berlier.v@gmail.com>
2019-03-11 01:46:52 -0400

    Add test for plugins order

--- tests/index.test.js
+++ tests/index.test.js
@@ -46,3 +46,14 @@ test('converts a long document successfully', () => {
 test('throws error with invalid xml', () => {
   expect(() => docutils.parse('invalid xml')).toThrow()
 })
+
+test('executes plugins in the right order', () => {
+  let count = 0
+
+  const first = () => count++
+  const second = () => expect(count++).toEqual(1)
+
+  docutils.parse(basicDoc, [first, second])
+
+  expect(count).toEqual(2)
+})
