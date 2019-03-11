1292a8936b2e9a82e37ba7c808c497be7b800a36
Valentin Berlier <berlier.v@gmail.com>
2019-03-11 01:57:50 -0400

    Rename tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,57 +3,60 @@ const docutils = require('../index')
 const basicDoc = require('./documents/hello.xml')
 const longDoc = require('./documents/index.xml')
 
-test('converts a basic document successfully', () => {
-  const result = docutils.parse(basicDoc)
-  expect(result).toEqual({
-    tag: 'document',
-    attributes: {
-      source: '.../hello.rst'
-    },
-    children: [
-      {
-        tag: 'section',
-        attributes: {
-          ids: 'hello-world',
-          names: 'hello,\\ world!'
-        },
-        children: [
-          {
-            tag: 'title',
-            attributes: {},
-            children: [
-              'Hello, world!'
-            ]
+describe('parse() function', () => {
+  test('basic document', () => {
+    const result = docutils.parse(basicDoc)
+    expect(result).toEqual({
+      tag: 'document',
+      attributes: {
+        source: '.../hello.rst'
+      },
+      children: [
+        {
+          tag: 'section',
+          attributes: {
+            ids: 'hello-world',
+            names: 'hello,\\ world!'
           },
-          {
-            tag: 'paragraph',
-            attributes: {},
-            children: [
-              'This file is empty.'
-            ]
-          }
-        ]
-      }
-    ]
+          children: [
+            {
+              tag: 'title',
+              attributes: {},
+              children: [
+                'Hello, world!'
+              ]
+            },
+            {
+              tag: 'paragraph',
+              attributes: {},
+              children: [
+                'This file is empty.'
+              ]
+            }
+          ]
+        }
+      ]
+    })
+  })
+
+  test('long document', () => {
+    const result = docutils.parse(longDoc)
+    expect(result).toMatchSnapshot()
+  })
+
+  test('invalid document', () => {
+    expect(() => docutils.parse('invalid xml')).toThrow()
   })
 })
 
-test('converts a long document successfully', () => {
-  const result = docutils.parse(longDoc)
-  expect(result).toMatchSnapshot()
-})
+describe('parser plugins', () => {
+  test('execution order', () => {
+    let count = 0
+    const first = () => count++
+    const second = () => expect(count++).toEqual(1)
 
-test('throws error with invalid xml', () => {
-  expect(() => docutils.parse('invalid xml')).toThrow()
-})
+    docutils.parse(basicDoc, [first, second])
 
-test('executes plugins in the right order', () => {
-  let count = 0
-
-  const first = () => count++
-  const second = () => expect(count++).toEqual(1)
-
-  docutils.parse(basicDoc, [first, second])
-
-  expect(count).toEqual(2)
+    expect(count).toEqual(2)
+  })
 })
